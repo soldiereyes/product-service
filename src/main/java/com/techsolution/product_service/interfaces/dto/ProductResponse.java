a746ada00df3c061558701696345adf062a41e8d
Guilherme Gonzaga Soares <guilhermegonzagasoares_@hotmail.com>
2025-12-19 14:28:13 -0300

    feat: cria o dto de response

---
+++ src/main/java/com/techsolution/product_service/interfaces/dto/ProductResponse.java
@@ -0,0 +1,14 @@
+package com.techsolution.product_service.interfaces.dto;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record ProductResponse(
+        UUID id,
+        String name,
+        String description,
+        BigDecimal price,
+        Integer stockQuantity
+) {
+}
+
