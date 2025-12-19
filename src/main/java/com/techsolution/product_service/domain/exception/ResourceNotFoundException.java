febf1e82c81174c9fd0e0be874fce768413dcf2c
Guilherme Gonzaga Soares <guilhermegonzagasoares_@hotmail.com>
2025-12-19 14:34:35 -0300

    feat: cria classe para camada de resource

---
+++ src/main/java/com/techsolution/product_service/domain/exception/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.techsolution.product_service.domain.exception;
+
+import java.util.UUID;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String resourceName, UUID id) {
+        super(String.format("%s with id %s not found", resourceName, id));
+    }
+}
+
