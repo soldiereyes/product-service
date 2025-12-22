34da89e0a4358d196a4faff1a34f5820f6992185
Guilherme Gonzaga Soares <guilhermegonzagasoares_@hotmail.com>
2025-12-21 23:08:36 -0300

    feat: limpeza da classe

--- src/main/java/com/techsolution/product_service/infrastructure/config/RedisConfig.java
+++ src/main/java/com/techsolution/product_service/infrastructure/config/RedisConfig.java
@@ -1,10 +1,5 @@
 package com.techsolution.product_service.infrastructure.config;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -13,10 +8,8 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 
@@ -50,6 +43,4 @@ public class RedisConfig {
                         defaultConfig.entryTtl(Duration.ofMinutes(3)))
                 .build();
     }
-
-
 }
